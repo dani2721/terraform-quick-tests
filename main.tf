91d9b1f911ead28a91b58c2d6e431f3ed4e58535
DANIEL ALEJANDRO PADRON LAINEZ <daniel_padron@es.ibm.com>
2021-09-21 11:53:37 +0200

    initial files

---
+++ main.tf
@@ -0,0 +1,17 @@
+
+# provider block required with Schematics to set VPC region
+provider "ibm" {
+  region = var.ibm_region
+  #ibmcloud_api_key = var.ibmcloud_api_key
+  generation = local.generation
+  version    = "~> 1.4"
+}
+
+data template_file "userdata" {
+  template = file("${path.module}/templates/userdata.yaml")
+
+  vars = {
+    username           = var.test_variable
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,10 @@
+
+# unique name for the VPC in the account 
+variable "test_variable" {
+  description = "test"
+  default     = "this-is-a-test"
+}
+
+
+
+
